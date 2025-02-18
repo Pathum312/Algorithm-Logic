d5b060bef817e7b5092111b01c33fc8ad041ca04
Pathum312 <pathum.2019557@iit.ac.lk>
2025-02-18 20:58:44 +0530

    Basic array operation

---
+++ Array/array.py
@@ -0,0 +1,14 @@
+print("\n### Basic Array Operations ###\n")
+
+# Initializing the numebr list
+number_list: list[int] = [1, 2, 3, 4]
+
+print(f"1. Initial Array: {number_list}\n")
+
+# Append 8 to the list
+number_list.append(8)
+print(f"2. Append 8: {number_list}\n")
+
+# Remove 2 from the list
+number_list.remove(2)
+print(f"3. Remove 2: {number_list}\n")
