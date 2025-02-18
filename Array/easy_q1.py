c5fd084d144ecfa54c17b3cc72456163f18cf913
Pathum312 <pathum.2019557@iit.ac.lk>
2025-02-18 21:12:10 +0530

    array easy question one

---
+++ Array/easy_q1.py
@@ -0,0 +1,18 @@
+"""
+Question:
+    Given an integer array arr[], print all distinct elements from this array. 
+    The given array may contain duplicates and the output should contain every element only once.
+    
+    Examples:
+        Input: arr_one: list[int] = [12, 10, 9, 45, 2, 10, 10, 45]
+        Output: [12, 10, 9, 45, 2]
+    
+        arr_two: list[int] = [1, 2, 3, 4, 5]
+        Output: [1, 2, 3, 4, 5]
+    
+        arr_three: list[int] = [1, 1, 1, 1, 1]
+        Output: [1]
+"""
+
+
+def find_distinct(arr: list[int]) -> list[int]: ...
