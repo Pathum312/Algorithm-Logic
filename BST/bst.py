4fbc90a1c0e1da4ae272fb1fd698d51b8c2e6c66
Pathum312 <pathum.2019557@iit.ac.lk>
2025-02-18 18:14:18 +0530

    formatted

--- BST/bst.py
+++ BST/bst.py
@@ -1,20 +1,21 @@
 class Node:
     """
     A class representing a node in a binary search tree
-    
+
     Attributes:
         key (int): The key of the node.
         left (Node | None): The left child of the node.
         right (Node | None): The right child of the node.
         parent (Node | None): The parent of the node.
     """
+
     def __init__(self, key: int) -> None:
         """
         Initialize the node with the given key
-        
+
         Parameters:
             key (int): The key of the node.
-            
+
         Returns:
             None
         """
@@ -23,38 +24,39 @@ class Node:
         self.right: Node | None = None
         self.parent: Node | None = None
 
+
 class BST:
     """
     A class representing a binary search tree
-    
+
     Attributes:
         root (Node): The root node of the tree.
     """
-    
+
     def __init__(self, key: int) -> None:
         """
         Initialize a root node with the given key
-        
+
         Parameters:
             key (int): The key of the root node.
-            
+
         Returns:
             None
         """
         self.root: Node = Node(key=key)
-    
+
     def insert(self, key: int) -> None:
         """
         Insert a new node with the given key into the tree
-        
+
         Parameters:
             key (int): The key of the new node.
-            
+
         Returns:
             None
         """
         node: Node = self.root
-        
+
         while True:
             if key < node.key:
                 if node.left is None:
@@ -68,120 +70,120 @@ class BST:
                     node.right.parent = node
                     break
                 node = node.right
-    
+
     def search(self, node: Node | None, key: int) -> Node | None:
         """
         Search for a node with the given key in the tree
-        
+
         Parameters:
             node (Node): The node to start the search from.
             key (int): The key to search for.
-            
+
         Returns:
             Node: The node with the given key if found, otherwise None
         """
         if node is None or node.key == key:
-            return node # type: ignore
-        
+            return node  # type: ignore
+
         if node.key > key:
             return self.search(node=node.left, key=key)
         else:
             return self.search(node=node.right, key=key)
-    
+
     def remove(self, key: int) -> None:
         """
         Remove a node with the given key from the tree
-        
+
         Parameters:
             key (int): The key of the node to remove.
-            
+
         Returns:
             None
         """
         found_node: Node | None = self.search(node=self.root, key=key)
-        
+
         if found_node is None:
-            print(f'\n{key} not found in the tree.')
+            print(f"\n{key} not found in the tree.")
             return
-        
+
         if found_node.left and found_node.right:
             self._remove_two_children(node=found_node)
         elif found_node.left or found_node.right:
             self._remove_one_child(node=found_node)
         else:
             self._remove_leaf(node=found_node)
-                
+
     def _remove_leaf(self, node: Node) -> None:
         """
         Remove a leaf node from the tree
-        
+
         Parameters:
             node (Node): The node to remove.
-            
+
         Returns:
             None
         """
-        parent: Node = node.parent # type: ignore
+        parent: Node = node.parent  # type: ignore
         if parent.left == node:
             parent.left = None
         else:
             parent.right = None
-            
+
     def _remove_one_child(self, node: Node) -> None:
         """
         Remove a node with one child from the tree
-        
+
         Parameters:
             node (Node): The node to remove.
-            
+
         Returns:
             None
         """
-        parent: Node = node.parent # type: ignore
-        child: Node = node.left if node.left else node.right # type: ignore
-        
+        parent: Node = node.parent  # type: ignore
+        child: Node = node.left if node.left else node.right  # type: ignore
+
         if parent:
             if parent.left == node:
                 parent.left = child
             else:
                 parent.right = child
-        
+
             child.parent = parent
         else:
             self.root = child
             self.root.parent = None
-    
+
     def _remove_two_children(self, node: Node) -> None:
         """
         Remove a node with two children from the tree
-        
+
         Parameters:
             node (Node): The node to remove.
-            
+
         Returns:
             None
         """
-        parent: Node = node.parent # type: ignore
-        successor: Node = self._get_successor(node=node if parent else node.right) # type: ignore
-        successor_parent: Node = successor.parent # type: ignore
-        
+        parent: Node = node.parent  # type: ignore
+        successor: Node = self._get_successor(node=node if parent else node.right)  # type: ignore
+        successor_parent: Node = successor.parent  # type: ignore
+
         node.key = successor.key
-        
+
         if successor_parent.left == successor:
             successor_parent.left = successor.right
         elif successor_parent.right == successor:
             successor_parent.right = successor.right
-        
+
         if successor.right:
             successor.right.parent = successor_parent
-    
+
     def _get_successor(self, node: Node) -> Node | None:
         """
         Get the successor node for the node being removed
-        
+
         Parameters:
             node (Node): The node to get the successor of.
-            
+
         Returns:
             Node: The successor node
         """
@@ -212,6 +214,7 @@ class BST:
             self.visualize(node=node.left, level=level + 1, prefix="L--- ")
             self.visualize(node=node.right, level=level + 1, prefix="R--- ")
 
+
 if __name__ == "__main__":
     # Create a binary search tree
     tree: BST = BST(key=20)
@@ -222,25 +225,25 @@ if __name__ == "__main__":
     tree.insert(key=15)
     tree.insert(key=25)
     tree.insert(key=35)
-    
+
     # Visualize the tree
     print("\nInitial")
     tree.visualize(node=tree.root)
-    
-    print('\nFound 30' if tree.search(node=tree.root, key=30) else '\n30 not found')
-    print('\nFound 28' if tree.search(node=tree.root, key=28) else '\n28 not found')
-    
+
+    print("\nFound 30" if tree.search(node=tree.root, key=30) else "\n30 not found")
+    print("\nFound 28" if tree.search(node=tree.root, key=28) else "\n28 not found")
+
     tree.remove(key=25)
-    
-    print('\nRemove 25')
+
+    print("\nRemove 25")
     tree.visualize(node=tree.root)
-    
+
     tree.remove(key=30)
-    
-    print('\nRemove 30')
+
+    print("\nRemove 30")
     tree.visualize(node=tree.root)
-    
+
     tree.remove(key=20)
-    
-    print('\nRemove 20')
+
+    print("\nRemove 20")
     tree.visualize(node=tree.root)
